b24660d3c55e489034a8e2b1bd3e0a99c8257f9b
JamesHaywood2 <haywoodjames2@gmail.com>
2024-02-07 16:33:08 -0600

    Image preview

--- Creator/GUI.py
+++ Creator/GUI.py
@@ -1,5 +1,6 @@
 import tkinter as tk
 import ImageSupport as IS
+from Widgets import PreviewImage
 
 
 
@@ -15,11 +16,11 @@ class Window:
         self.root.geometry(f"{screen_width//2}x{screen_height//2}+{screen_width//4}+{screen_height//4}")
         
         #Base PanedWindow that holds everything. Takes up the whole window and should always be the same size as the window
-        self.base = tk.PanedWindow(self.root, orient=tk.VERTICAL, bd=5, sashwidth=8)
+        self.base = tk.PanedWindow(self.root, orient=tk.VERTICAL, bd=1, sashwidth=10)
         self.base.pack(expand=True, fill="both")
 
         #Top PanedWindow that houses media bucket and preview. 
-        self.top = tk.PanedWindow(self.base, orient=tk.HORIZONTAL, bd=5, sashwidth=8)
+        self.top = tk.PanedWindow(self.base, orient=tk.HORIZONTAL, bd=1, sashwidth=10)
         self.base.add(self.top)
 
         #Bottom frame that takes up 1/3 of the window
@@ -33,32 +34,64 @@ class Window:
         self.top.add(self.preview)
         
         #Get the size of the window
-        # self.root.update()
+        self.root.update()
         self.win_width = self.root.winfo_width()
         self.win_height = self.root.winfo_height()
 
+        #Sets the initial size of the PanedWindows
         self.__sizeConfig()
+
+        #Add an ImagePreview to the preview frame
+        self.previewImage = PreviewImage(self.preview)
+        
+
+        #DEBUG STUFF REMOVE LATER
+        printCanvasSize = tk.Button(self.reel, text="Print Canvas Size", command=self.previewImage.printCanvasSize)
+        printCanvasSize.pack()
+        redrawImage = tk.Button(self.reel, text="Redraw Image", command=self.previewImage.redrawImage)
+        redrawImage.pack()
+        #DEBUG STUFF REMOVE LATER
+
+        #Get the position of the window
+        self.win_xPos = self.root.winfo_x()
+        self.win_yPos = self.root.winfo_y()
+
+        #resize after call variable.
+        self.resize_after = None
+
 
         #Bind the resize event to the on_resize function
         self.root.bind("<Configure>", self.on_resize)
         self.root.mainloop()
 
-    #Right now it just prints the size of the window. Updates whenever anything is resized including the PanedWindows.
+    #Debounce function. On_resize() gets called every <configure> event and creates a new after event.
+    #If it gets called again, the after event gets cancelled and a new one is created.
+    #If it doesn't it then calls the resize function.
+    #This prevents the resize function from being spam and causing lag.
     def on_resize(self, event):
+        if self.resize_after:
+            self.root.after_cancel(self.resize_after)
+        self.resize_after = self.root.after(50, self.resize, event)
+        
+    def resize(self, event):
+        print("Resizing")
         self.win_width = self.root.winfo_width()
         self.win_height = self.root.winfo_height()
 
-
+        
         #This is to prevetn an infinite loop of resizing. pane.configure() ends up triggering the on_resize event, which triggers the pane.configure() event, etc.
         #This snippet of code will only run if the window itself has changed size, not any of the PanedWindows.
         if (self.win_width == event.width) and (self.win_height == event.height):
             # print(f"Current Window Size: {event.width}x{event.height}")
             self.__sizeConfig()
+            # print(f"Window Size: {self.win_width}x{self.win_height}")
 
-            return
-
-
+        #If canvas size has changed, redraw the image
+        if self.previewImage.canvas.winfo_width() != self.previewImage.canvasWidth or self.previewImage.canvas.winfo_height() != self.previewImage.canvasHeight:
+            self.previewImage.redrawImage()
+                
         return
+
     
     def __sizeConfig(self):
         #Top PanedWindow should take up 2/3 of the window
@@ -67,11 +100,11 @@ class Window:
         #Bottom frame should take up 1/3 of the window
         self.base.paneconfigure(self.reel, height=self.win_height//3, minsize=self.win_height//4)
 
-        #Media bucket should take up 1/3 of the top PanedWindow
-        self.top.paneconfigure(self.media, width=self.win_width//3, minsize=self.win_width//4)
-
         #Preview should take up 2/3 of the top PanedWindow
         self.top.paneconfigure(self.preview, width=self.win_width//3*2, minsize=self.win_width//3)
+
+        #Media bucket should take up 1/3 of the top PanedWindow
+        self.top.paneconfigure(self.media, width=self.win_width//3, minsize=self.win_width//4)
         return
 
 
--- Creator/Widgets.py
+++ Creator/Widgets.py
@@ -7,7 +7,7 @@ import ImageSupport as IS
 class PreviewImage:
     #PreviewImage is essentially a canvas that holds an image.
     def __init__(self, parent):
-        self.canvas = tk.Canvas(parent)
+        self.canvas = tk.Canvas(parent, bg="black")
         #Should fill the parent as much as it can.
         self.canvas.pack(fill=tk.BOTH, expand=True)
         #Get the size of the canvas
@@ -20,6 +20,7 @@ class PreviewImage:
         self.imagePIL = None
         self.image = None
         self.canvasImage = None
+
 
 
     def loadImage(self, imagePath:str):
@@ -42,21 +43,47 @@ class PreviewImage:
         self.canvasImage = self.canvas.create_image(self.canvasWidth//2, self.canvasHeight//2, image=self.image)
 
 
+    def redrawImage(self):
+        #Return early if there is no image
+        if self.imagePath == None:
+            return
+
+        #Get the size of the canvas
+        self.canvasWidth = self.canvas.winfo_width()
+        self.canvasHeight = self.canvas.winfo_height()
+        # print(f"Current Window Size: {event.width}x{event.height}")
+        # print(f"Canvas Size: {self.canvasWidth}x{self.canvasHeight}")
+        #Clear the canvas
+        self.canvas.delete("all")
+        #Resize the image while using the aspect ratio
+        img = Image.open(self.imagePath)
+        self.imagePIL = img
+        self.imagePIL.thumbnail((self.canvasWidth, self.canvasHeight))
+        self.image = ImageTk.PhotoImage(self.imagePIL)
+        self.canvasImage = self.canvas.create_image(self.canvasWidth//2, self.canvasHeight//2, image=self.image)
+        return
+
+    def printCanvasSize(self):
+        self.canvasWidth = self.canvas.winfo_width()
+        self.canvasHeight = self.canvas.winfo_height()
+        print(f"Canvas Size: {self.canvasWidth}x{self.canvasHeight}")
+
+
 
 
 # Create the root window for testing
-root = tk.Tk()
-root.title("Test window")
-screen_width = root.winfo_screenwidth()
-screen_height = root.winfo_screenheight()
-root.geometry(f"{screen_width//2}x{screen_height//2}+{screen_width//4}+{screen_height//4}")
-print(f"{screen_width//2}x{screen_height//2}")
+# root = tk.Tk()
+# root.title("Test window")
+# screen_width = root.winfo_screenwidth()
+# screen_height = root.winfo_screenheight()
+# root.geometry(f"{screen_width//2}x{screen_height//2}+{screen_width//4}+{screen_height//4}")
+# print(f"{screen_width//2}x{screen_height//2}")
 
 
-#Create the preview image
-pi = PreviewImage(root)
-pi.loadImage(r"Slideshow-Project\ball2.jpg")
+# #Create the preview image
+# pi = PreviewImage(root)
+# pi.loadImage(r"Slideshow-Project\ball.jpg")
 
 
 
-root.mainloop()
+# root.mainloop()
